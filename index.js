3e9457bac8b9008d3eb23c79002dd95f394c9cc9
Adam MacDonald <adam.macdonald-corrao@vistana.com>
2018-02-23 11:30:20 -0500

    Revised static gets for revised folder structure

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const express = require('express'),
 	path = require('path'),
 	app = express(),
-	Tree = require('./src/models/Tree');
+	Tree = require('./api/src/models/Tree');
 
 // serving files for preview application
-app.use('/', express.static('preview'));
+app.use('/', express.static('preview/public'));
 
 // serving emails that are built with the workflow
-app.use('/emails', express.static('emails'));
+app.use('/emails', express.static('emails/build'));
 
 // serve json that displays the full file tree for built emails
-app.get('/tree', (req, res) => {
-	const filetree = new Tree('emails');
+app.get('/api/tree', (req, res) => {
+	const filetree = new Tree('emails/build');
 	res.json(filetree.getTree());
 });
 
