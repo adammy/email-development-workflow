7344dfc93faa7545c9a16c569436c3915b9d4679
Adam MacDonald <adam.macdonald-corrao@vistana.com>
2018-02-23 09:25:22 -0500

    Remove json spacing attr and log of tree access

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ const express = require('express'),
 	app = express(),
 	Tree = require('./src/models/Tree');
 
-// attribute for prettifying json; development purposes only
-app.set('json spaces', 2);
-
 // serving files for preview application
 app.use('/', express.static('preview'));
 
@@ -14,11 +11,6 @@ app.use('/emails', express.static('emails'));
 
 // serve json that displays the full file tree for built emails
 app.get('/tree', (req, res) => {
-	console.log("File tree accessed");
-	// res.json({
-	// 	name: "testy",
-	// 	age: 22
-	// });
 	const filetree = new Tree('emails');
 	res.json(filetree.getTree());
 });
