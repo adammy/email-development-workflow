38b2729132bba227482fe5923b219bcf1df05d72
Adam MacDonald <adam.macdonald-corrao@vistana.com>
2018-02-23 16:49:39 -0500

    Parsing tree data to create a view and styling for view using flex

--- preview/src/App.js
+++ preview/src/App.js
@@ -9,22 +9,77 @@ class App extends Component {
 		this.state = {
 			files: [
 			  {
-			    "transactional": [
+			    "name": "random-in-root.html",
+			    "dir": "emails\/build",
+			    "created": "2018-02-23T21:13:06.000Z",
+			    "modified": "2018-02-23T21:13:06.000Z",
+			    "size": 0,
+			    "subject": "Undefined",
+			    "preview": "Undefined"
+			  },
+			  {
+			    "name": "random-in-root.txt",
+			    "dir": "emails\/build",
+			    "created": "2018-02-23T21:13:12.897Z",
+			    "modified": "2018-02-23T21:13:12.897Z",
+			    "size": 0
+			  },
+			  {
+			    "sales": [
 			      {
-			        "name": "receipt.html",
-			        "dir": "emails\/build\/transactional",
-			        "created": "2018-02-23T16:40:47.618Z",
-			        "modified": "2018-02-23T16:40:47.618Z",
+			        "name": "package.html",
+			        "dir": "emails\/build\/sales",
+			        "created": "2018-02-23T21:13:22.302Z",
+			        "modified": "2018-02-23T21:13:22.302Z",
 			        "size": 0,
 			        "subject": "Undefined",
 			        "preview": "Undefined"
+			      }
+			    ]
+			  },
+			  {
+			    "transactional": [
+			      {
+			        "auto-response": [
+			          {
+			            "name": "res-week.html",
+			            "dir": "emails\/build\/transactional\/auto-response",
+			            "created": "2018-02-23T21:12:53.557Z",
+			            "modified": "2018-02-23T21:12:53.557Z",
+			            "size": 0,
+			            "subject": "Undefined",
+			            "preview": "Undefined"
+			          },
+			          {
+			            "name": "res.html",
+			            "dir": "emails\/build\/transactional\/auto-response",
+			            "created": "2018-02-23T21:12:41.373Z",
+			            "modified": "2018-02-23T21:12:41.373Z",
+			            "size": 0,
+			            "subject": "Undefined",
+			            "preview": "Undefined"
+			          }
+			        ]
 			      },
 			      {
-			        "name": "receipt.txt",
-			        "dir": "emails\/build\/transactional",
-			        "created": "2018-02-23T16:40:53.501Z",
-			        "modified": "2018-02-23T16:40:53.501Z",
-			        "size": 0
+			        "receipt": [
+			          {
+			            "name": "receipt.html",
+			            "dir": "emails\/build\/transactional\/receipt",
+			            "created": "2018-02-23T16:40:47.618Z",
+			            "modified": "2018-02-23T16:40:47.618Z",
+			            "size": 0,
+			            "subject": "Undefined",
+			            "preview": "Undefined"
+			          },
+			          {
+			            "name": "receipt.txt",
+			            "dir": "emails\/build\/transactional\/receipt",
+			            "created": "2018-02-23T16:40:53.501Z",
+			            "modified": "2018-02-23T16:40:53.501Z",
+			            "size": 0
+			          }
+			        ]
 			      }
 			    ]
 			  }
@@ -36,6 +91,8 @@ class App extends Component {
 		return (
 			<div className="app">
 				<Tree files={this.state.files} />
+				<div className="preview">
+				</div>
 			</div>
 		);
 	}
--- preview/src/components/Tree/index.js
+++ preview/src/components/Tree/index.js
@@ -3,18 +3,36 @@ import PropTypes from 'prop-types';
 import './style.css';
 
 const Tree = ({ files = [] }) => {
+
+	function showFile(file) {
+		if (file.subject) { // html file
+			return (
+				<button className="file file-email">
+					{file.name}
+				</button>
+			);
+		} else if (file.name) { // file (not html)
+			return (
+				<button className="file file-misc">
+					{file.name}
+				</button>
+			);
+		} else { // folder
+			const name = Object.keys(file)[0];
+			return (
+				<button className="file file-folder">
+					{name}
+				</button>
+			);
+		}
+	}
+
 	return (
-		<div>
-			<h1>Files</h1>
-			{files.map(file => {
-				return file[Object.keys(file)].map(file => {
-					return (
-						<h2>{file.name}</h2>
-					);
-				});
-			})}
+		<div className="tree">
+			{files.map(file => showFile(file))}
 		</div>
 	);
+
 };
 
 Tree.propTypes = {
