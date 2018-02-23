55a18226ba43fe4500f4ba83447cc57c7b3cb7f9
Adam MacDonald <adam.macdonald-corrao@vistana.com>
2018-02-23 11:29:52 -0500

    Initial setup of react preview app

---
+++ preview/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			user: 'Adam'
+		};
+	}
+
+	render() {
+		return (
+			<div className="app">
+				<h1>dsf</h1>
+			</div>
+		);
+	}
+
+}
+
+export default App;
